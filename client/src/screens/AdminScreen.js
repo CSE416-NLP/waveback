a17c5017d43e64b2ec4d69db91e25b1009c25a07
NathanWine <illseeyouafterclass@gmail.com>
2020-10-16 02:42:50 -0400

    Fixed issue with root not taking remaining space/took much space from Navbar

--- client/src/screens/AdminScreen.js
+++ client/src/screens/AdminScreen.js
@@ -6,15 +6,11 @@ import "../styles/css/index.css"
 const AdminScreen = (props) => {
   return (
     <div style={{ height: "100%", textAlign: "center"}}>
-      <div style={{ backgroundColor: "var(--primary)", height: "15%"}}>This is the admin control panel. This should be changed so it is only accessable from admin accounts</div>
-      <div style={{ backgroundColor: "var(--secondary)", height: "15%" }}>TEST APIs</div>
-      <div style={{ backgroundColor: "var(--tertiary)", height: "15%" }}>Remember to brush your teeth</div>
-      <div style={{ backgroundColor: "var(--accent)", height: "15%" }}>And clean behind your ears</div>
-      <div style={{ backgroundColor: "var(--background)", height: "15%" }}>When will the SRS be graded? :(</div>
-      {props.location.beter && <p>BETER ZENG HAHA</p>}
-      <div>
-        <Link to="/">Go back home</Link>
-      </div>
+      <div style={{ backgroundColor: "var(--primary)", height: "20%"}}>This is the admin control panel. This should be changed so it is only accessable from admin accounts {props.location.beter && <p>BETER ZENG HAHA</p>}</div>
+      <div style={{ backgroundColor: "var(--secondary)", height: "20%" }}>TEST APIs</div>
+      <div style={{ backgroundColor: "var(--tertiary)", height: "20%" }}>Remember to brush your teeth</div>
+      <div style={{ backgroundColor: "var(--accent)", height: "20%" }}>And clean behind your ears</div>
+      <div style={{ backgroundColor: "var(--background)", height: "20%" }}>When will the SRS be graded? :(</div>
     </div>
   );
 };
