037a8303873943a78e5a3f21c45c89587efdf994
NathanWine <illseeyouafterclass@gmail.com>
2020-10-17 15:25:50 -0400

    Adjusted navbar image styling to allow for different heights

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -7,19 +7,19 @@ import tempProfileImage from './images/temporary profile image.png';
 const Navbar = (props) => {
   return (
     <ul className="navbar">
-        <Link to="/">
+        <Link className="navbar_link" to="/">
             <li className="navbar_item"> <img src={wavebackCircleLogo} className="App-logo-navbar" alt="r" /> </li>
         </Link>
-        <Link to="/Discover">
+        <Link className="navbar_link" to="/Discover">
             <li className="navbar_item"> Discover </li>
         </Link>
-        <Link to="/Generate">
+        <Link className="navbar_link" to="/Generate">
             <li className="navbar_item"> Generate </li>
         </Link>
-        <Link to="/Playlists">
+        <Link className="navbar_link" to="/Playlists">
             <li className="navbar_item"> Playlists </li>
         </Link>
-        <Link to="/Profile">
+        <Link className="navbar_link" to="/Profile">
             <li className="navbar_item"> <img src={tempProfileImage} className="App-logo-navbar" alt="r" /> </li>
         </Link>
     </ul>
