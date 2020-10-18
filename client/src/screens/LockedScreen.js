a50640726e8c66c5d4d9e68473ded44888053670
Peter Zeng <peterateast@gmail.com>
2020-10-18 18:08:45 -0400

    added the start of searching using spotify api

--- client/src/screens/LockedScreen.js
+++ client/src/screens/LockedScreen.js
@@ -9,9 +9,9 @@ const LockedScreen = (props) => {
     // This is an example of only allowing the user to visit this page if some requirement is met
     // if (!props.location.key)
     //     return <Redirect to="/" />
-    
+
     const [data, setData] = useState(null);
-    
+
     const queryAPI = (songID) => {
         // Nate's YouTube API key is AIzaSyBqoZTfsx0o7_2UBNBPKOMGRREY6Qg2wAc
         let APIKey = "&key=AIzaSyBqoZTfsx0o7_2UBNBPKOMGRREY6Qg2wAc";
@@ -21,13 +21,32 @@ const LockedScreen = (props) => {
         fetch(query, {                      // Attempt to get some data from YouTube
             method: "GET",
         })
-        .then(response => response.json())  // Turn the reponse into JSON representation
-        .then(data => {
-            if (data && data.items && data.items[0]) {
-                console.log(data.items[0])
-                setData(data.items[0]);     // Use the JSON however we see fit
+            .then(response => response.json())  // Turn the reponse into JSON representation
+            .then(data => {
+                if (data && data.items && data.items[0]) {
+                    console.log(data.items[0])
+                    setData(data.items[0]);     // Use the JSON however we see fit
+                }
+            });
+    }
+
+    const onClickHandler = (term) => { // Default is all fields, playlist generation may only use song search
+        let token = "BQDPfiFlkR4DvvbZ19-vpSZSU76b7frlhHm4Xu1IGWK1VAnhL70GG8rI4-20_gNgVmqRHXBB-AzI8zy9O2eV8AGASZRr2ztiWGeF7aWmE2WpjKOkHiZy3x6d5wff-xWqko6V-0CTuCruFEyBz5siHW-_Dm0odhkRNFpcS8rI7t28e6NkFWSrDjTKtmMIBbQxG7OyRqm4Vm-eDfd6D-E30LtTM8xDc1Vx9a17xfRXdATO0xN6NLooIxbesyKCHhCSAikN5grqEw8_jBUzTgfO";
+        token = "Bearer "+token;
+        let query = "https://api.spotify.com/v1/search?q=" + term + "&type=track%2Cartist%2Calbum&market=US"
+        
+        fetch(query, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": token
             }
-        });
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            });
     }
 
     const parseTime = (ISO_8601_String) => {
@@ -58,14 +77,20 @@ const LockedScreen = (props) => {
         // console.log(seconds)
     }
 
+    const [searchTerm, setSearch] = useState("");
     return (
-        <div className="App" style={{flex: "1", display: "flex", flexDirection: "column"}}>
+        <div className="App" style={{ flex: "1", display: "flex", flexDirection: "column" }}>
             {/* <p>Can only get here from clicking link, not directly from url. Try setting the theme in admin and then coming back here!</p> */}
             <div>
                 <button onClick={() => queryAPI("-tJYN-eG1zk")}>Play Queen</button>
                 <button onClick={() => queryAPI("-cmSCQbWxV0")}>Play Kanye</button>
+                <div>
+                    <input value={searchTerm} onChange={(e) => setSearch(e.target.value)} type="text" placeholder="Search.." class="discoverSearch"></input>
+                    <button onClick={() => onClickHandler(searchTerm)}>Query Data in Console</button>
+                </div>
+
             </div>
-            {data && <div style={{flex: "1"}}>
+            {data && <div style={{ flex: "1" }}>
                 <iframe src={"http://www.youtube.com/embed/" + data.id}
                     width="100%" height="100%" frameBorder="0" allowFullScreen={true} />
             </div>}
