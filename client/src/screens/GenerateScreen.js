7aea6f9d8a1659ccf4a7ea2aac54eec7419102b5
unknown <peterateast@gmail.com>
2020-10-21 10:25:44 -0400

    fixed some of the weird html DOM validation, and changed the incrementers/decrementers

--- client/src/screens/GenerateScreen.js
+++ client/src/screens/GenerateScreen.js
@@ -13,6 +13,8 @@ const GenerateScreen = (props) => {
     genre: "",
   }]);
 
+  const [num_songs, setNum_songs] = useState(0);
+
   const addLocation = () => {
     let locationCopy = [...locationState];
     locationCopy.push({
@@ -51,17 +53,19 @@ const GenerateScreen = (props) => {
     setGenres(genreCopy);
   }
 
-  const songIncrement1 = () => {
-    let val = document.getElementById("songNumberInput").value;
-    if (val == null) { document.getElementById("songNumberInput").value = 1; }
-    else { document.getElementById("songNumberInput").value++; }
-  }
+  // const songIncrement1 = () => {
+  //   // let val = document.getElementById("songNumberInput").value;
+  //   // if (val == null) { document.getElementById("songNumberInput").value = 1; }
+  //   // else { document.getElementById("songNumberInput").value++; }
 
-  const songDecrement1 = () => {
-    let val = document.getElementById("songNumberInput").value;
-    if (val == null || val == 0) { document.getElementById("songNumberInput").value = "0"; }
-    else { document.getElementById("songNumberInput").value--; }
-  }
+  // }
+
+  // const songDecrement1 = () => {
+  //   // let val = document.getElementById("songNumberInput").value;
+  //   // if (val == null || val === 0) { document.getElementById("songNumberInput").value = "0"; }
+  //   // else { document.getElementById("songNumberInput").value--; }
+
+  // }
 
   return (
     <div className="generateScreen">
@@ -69,49 +73,49 @@ const GenerateScreen = (props) => {
 
       <div className="generateScreenModules">
         <p className="generateScreenText">Locations
-          <button className="ui primary icon button generateSquareButton" onClick={addLocation}><i class="plus circle icon"></i></button>
-          <button className="ui grey icon button generateSquareButton" onClick={subtractLocation}><i class="minus circle icon"></i></button>
+          <button className="ui primary icon button generateSquareButton" onClick={addLocation}><i className="plus circle icon"></i></button>
+          <button className="ui grey icon button generateSquareButton" onClick={subtractLocation}><i className="minus circle icon"></i></button>
         </p>
         <div align="center" className="generateScreenBox" id="gsbLeft">
           <i id="locationText1">Location</i><i id="locationText2">Year Start</i><i id="locationText3">Year End</i>
-            <div className="generateLeftScroller">
-              {locationState.map((row, index) =>
-              <p key={index}>
+          <div className="generateLeftScroller">
+            {locationState.map((row, index) =>
+              <div key={index}>
                 <div className="ui input">
                   <input size="32" value={row.location} className="generateInput" onChange={(e) => addLocationInfo(index, "location", e)} />
                 </div>
                 <div className="ui input">
-                  <input size="4"  maxLength="4" value={row.startYear} className="generateInput" onChange={(e) => addLocationInfo(index, "startYear", e)} />
+                  <input size="4" maxLength="4" value={row.startYear} className="generateInput" onChange={(e) => addLocationInfo(index, "startYear", e)} />
                 </div>
                 <div className="ui input">
                   <input size="4" maxLength="4" value={row.endYear} className="generateInput" onChange={(e) => addLocationInfo(index, "endYear", e)} />
                 </div>
-              </p>
-              )}
-            </div> 
+              </div>
+            )}
+          </div>
         </div>
       </div>
 
       <div className="generateScreenModules">
         <div>
           <p className="generateScreenText">Genres
-          <button className="ui icon primary button generateSquareButton" onClick={addGenre}><i class="plus circle icon"></i></button>
-          <button className="ui icon grey button generateSquareButton" onClick={subtractGenre}><i class="minus circle icon"></i></button>
+          <button className="ui icon primary button generateSquareButton" onClick={addGenre}><i className="plus circle icon"></i></button>
+            <button className="ui icon grey button generateSquareButton" onClick={subtractGenre}><i className="minus circle icon"></i></button>
           </p>
 
           <div align="center" className="generateScreenBox" id="gsbRight">
 
-            <p className="genreLeft">
+            <div className="genreLeft">
               <i id="genreText1">Search for Genre</i>
               <div className="ui input"><input size="20" id="genreSearch" className="generateInputGenre" /></div>
-              
-            </p>
+
+            </div>
 
             <div className="generateRightScroller">
               {genreState.map((row, index) =>
-              <p key={index}>
+                <p key={index}>
                   <i className="genreList">{row.genre}</i>
-              </p>
+                </p>
               )}
             </div>
 
@@ -121,16 +125,16 @@ const GenerateScreen = (props) => {
         <div className="generateBottomArea">
           <p className="generateScreenText">Preferred Number of Songs</p>
           <div className="ui input maxSongInputArea">
-            <button class="ui grey icon button"><i class="angle left icon" onClick={songDecrement1}></i></button>
-            <input size="1" maxLength="2" id="songNumberInput"/>
-            <button class="ui grey icon button"><i class="angle right icon" onClick={songIncrement1}></i></button>
+            <button className="ui grey icon button"><i className="angle left icon" onClick={(e) => setNum_songs(num_songs - 1)} ></i></button>
+            <input size="1" maxLength="2" id="songNumberInput" value={num_songs} onChange={(e) => setNum_songs(parseInt(e.target.value))} />
+            <button className="ui grey icon button"><i className="angle right icon" onClick={(e) => setNum_songs(num_songs + 1)}></i></button>
           </div>
         </div>
 
         <div className="generateButtonBox"><button className="ui primary button generateButton">GENERATE!</button></div>
 
       </div>
-        
+
     </div>
   );
 };
