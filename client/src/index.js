3472bc42ae9b910b904b339945a3736a8019fafb
TLeonBrown <tianbrown@gmail.com>
2020-10-20 22:31:07 -0400

    Revamp Generate screen with Semantic-UI!

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 // import resolvers from './cache/resolvers';   // Uncomment when we have resolvers
 import App from './App';
+import 'semantic-ui-css/semantic.min.css';
 import './styles/css/index.css';
 
 const cache = new InMemoryCache({ dataIdFromObject: object => `${object.__typename}:${object._id}`, });
--- client/src/screens/GenerateScreen.js
+++ client/src/screens/GenerateScreen.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { VscDiffAdded, VscDiffRemoved } from "react-icons/vsc";
 import "../styles/css/index.css"
 
 const GenerateScreen = (props) => {
@@ -38,9 +37,11 @@ const GenerateScreen = (props) => {
 
   const addGenre = () => {
     let genreCopy = [...genreState];
-    genreCopy.push({
-      genre: "",
-    });
+    if (document.getElementById("genreSearch").value.length > 0) {
+      genreCopy.push({
+        genre: document.getElementById("genreSearch").value,
+      });
+    }
     setGenres(genreCopy);
   }
 
@@ -50,50 +51,66 @@ const GenerateScreen = (props) => {
     setGenres(genreCopy);
   }
 
+  const songIncrement1 = () => {
+    let val = document.getElementById("songNumberInput").value;
+    if (val == null) { document.getElementById("songNumberInput").value = 1; }
+    else { document.getElementById("songNumberInput").value++; }
+  }
+
+  const songDecrement1 = () => {
+    let val = document.getElementById("songNumberInput").value;
+    if (val == null || val == 0) { document.getElementById("songNumberInput").value = "0"; }
+    else { document.getElementById("songNumberInput").value--; }
+  }
+
   return (
     <div className="generateScreen">
-
       <div className="generateScreenTitleText">bring back the sounds of...</div>
 
       <div className="generateScreenModules">
         <p className="generateScreenText">Locations
-          <button className="generateSquareButton" onClick={addLocation}><VscDiffAdded /></button>
-          <button className="generateSquareButton" onClick={subtractLocation}><VscDiffRemoved /></button>
+          <button className="ui primary icon button generateSquareButton" onClick={addLocation}><i class="plus circle icon"></i></button>
+          <button className="ui grey icon button generateSquareButton" onClick={subtractLocation}><i class="minus circle icon"></i></button>
         </p>
         <div align="center" className="generateScreenBox" id="gsbLeft">
-          <div className="generateLeftScroller">
-            <i id="locationText1">Location</i><i id="locationText2">Year Start</i><i id="locationText3">Year End</i>
-            {locationState.map((row, index) =>
-            <p key={index}>
-              <input size="33" value={row.location} className="generateInput" onChange={(e) => addLocationInfo(index, "location", e)} />
-              <input size="3"  maxLength="4" value={row.startYear} className="generateInput" onChange={(e) => addLocationInfo(index, "startYear", e)} />
-              <input size="3" maxLength="4" value={row.endYear} className="generateInput" onChange={(e) => addLocationInfo(index, "endYear", e)} />
-            </p>
-            )}
-          </div> 
+          <i id="locationText1">Location</i><i id="locationText2">Year Start</i><i id="locationText3">Year End</i>
+            <div className="generateLeftScroller">
+              {locationState.map((row, index) =>
+              <p key={index}>
+                <div className="ui input">
+                  <input size="32" value={row.location} className="generateInput" onChange={(e) => addLocationInfo(index, "location", e)} />
+                </div>
+                <div className="ui input">
+                  <input size="4"  maxLength="4" value={row.startYear} className="generateInput" onChange={(e) => addLocationInfo(index, "startYear", e)} />
+                </div>
+                <div className="ui input">
+                  <input size="4" maxLength="4" value={row.endYear} className="generateInput" onChange={(e) => addLocationInfo(index, "endYear", e)} />
+                </div>
+              </p>
+              )}
+            </div> 
         </div>
       </div>
 
       <div className="generateScreenModules">
         <div>
           <p className="generateScreenText">Genres
-            <button className="generateSquareButton" onClick={addGenre}><VscDiffAdded /></button>
-            <button className="generateSquareButton" onClick={subtractGenre}><VscDiffRemoved /></button>
+          <button className="ui icon primary button generateSquareButton" onClick={addGenre}><i class="plus circle icon"></i></button>
+          <button className="ui icon grey button generateSquareButton" onClick={subtractGenre}><i class="minus circle icon"></i></button>
           </p>
 
           <div align="center" className="generateScreenBox" id="gsbRight">
-            <div align="center" className="generateScreenLine" id="gsbRightVert"></div>
 
             <p className="genreLeft">
               <i id="genreText1">Search for Genre</i>
-              <br></br><br></br>
-              <input size="20" className="generateInputGenre" />
+              <div className="ui input"><input size="20" id="genreSearch" className="generateInputGenre" /></div>
+              
             </p>
 
             <div className="generateRightScroller">
               {genreState.map((row, index) =>
               <p key={index}>
-                  <i className="genreList">beter zeng</i>
+                  <i className="genreList">{row.genre}</i>
               </p>
               )}
             </div>
@@ -101,13 +118,16 @@ const GenerateScreen = (props) => {
           </div>
         </div>
 
-
-        <div>
+        <div className="generateBottomArea">
           <p className="generateScreenText">Preferred Number of Songs</p>
-          <p align="center" className="generateScreenBox" id="gsbBottom"></p>
+          <div className="ui input maxSongInputArea">
+            <button class="ui grey icon button"><i class="angle left icon" onClick={songDecrement1}></i></button>
+            <input size="1" maxLength="2" id="songNumberInput"/>
+            <button class="ui grey icon button"><i class="angle right icon" onClick={songIncrement1}></i></button>
+          </div>
         </div>
 
-        <button className="generateButton">GENERATE!</button>
+        <div className="generateButtonBox"><button className="ui primary button generateButton">GENERATE!</button></div>
 
       </div>
         
