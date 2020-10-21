723b8b36792538fb79463774b9e1efa24a1d03f7
TLeonBrown <tianbrown@gmail.com>
2020-10-21 18:22:03 -0400

    Start making Splash screen nicer.

--- client/src/screens/SplashScreen.js
+++ client/src/screens/SplashScreen.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { COLOR_SCHEMES } from '../styles/colorSchemes'
 import '../styles/css/index.css';
-import wavebackTextLogo from '../images/WB Text Logo 1.png';
+import wavebackTextFG from '../images/waveback text fg.png';
+import wavebackTextBG from '../images/waveback text bg.png';
+
+const changeStyle = (style) => {
+    document.documentElement.style.setProperty("--primary", style.primary);
+    document.documentElement.style.setProperty("--secondary", style.secondary);
+    document.documentElement.style.setProperty("--tertiary", style.tertiary);
+    document.documentElement.style.setProperty("--accent", style.accent);
+    document.documentElement.style.setProperty("--background", style.background);
+  }
 
 const handleLogin = () => {
     console.log("login");
@@ -17,19 +27,33 @@ const handleForgotPassword = () => {
 const SplashScreen = (props) => {
     return (
         <div className="App">
-            <header className="App-header">
+            <header className="App-header" style={{backgroundColor: "var(--background)"}}>
                 <p>
-                    <img src={wavebackTextLogo} className="App-logo" alt="r" />
+                    <img src={wavebackTextBG} className="appLogoBG" alt="r" />
+                    <img src={wavebackTextFG} className="appLogoFG" alt="r" />     
                 </p>
-                {/* We can also pass just the path rather than an entire location object, 
-                    depends on if we have additonal fields we want to pass in. */}
-                        
-                <div onClick = {handleLogin} className = "navbar_item ripple">Login</div>
-                <div onClick = {handleRegister} className = "navbar_item ripple" >Register</div>
-                <div onClick = {handleForgotPassword} className = "navbar_item ripple" >Forgot Password?</div>
+
+                <div>
+                    <div onClick = {handleLogin} className="splashText ripple" style={{color: "var(--accent)"}}>login</div>
+                    <div onClick = {handleRegister} className="splashText ripple" style={{color: "var(--accent)"}}>register</div>
+                    <div onClick = {handleForgotPassword} className="splashText ripple" style={{color: "var(--accent)"}}>forgot password?</div>
+                </div>
                 
-                <span className="clickable" onClick={() => props.history.push({ pathname: "/admin", key: true })}>Go to admin</span>
-                <span className="clickable" onClick={() => props.history.push({ pathname: "/test", key: true })}>Go to test</span>
+                <div className="splashTestSelect">
+                    <span className="clickable" onClick={() => props.history.push({ pathname: "/admin", key: true })}>Go to admin</span>
+                    <span className="clickable" onClick={() => props.history.push({ pathname: "/test", key: true })}>Go to test</span>
+                    <select onChange={(e) => {
+                        changeStyle(COLOR_SCHEMES[e.target.value])
+                    }}>
+                        <option value={"1950s"}>1950s</option>
+                        <option value={"1960s"}>1960s</option>
+                        <option value={"1970s"}>1970s</option>
+                        <option value={"1980s"}>1980s</option>
+                        <option value={"1990s"}>1990s</option>
+                        <option value={"2000s"}>2000s</option>
+                        <option value={"2010s"}>2010s</option>
+                    </select>
+                </div>
             </header>
         </div>
     );
