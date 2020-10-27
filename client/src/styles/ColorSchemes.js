863b88a2b13d8b721fb2386ff67ce3d84bdd267d
NathanWine <illseeyouafterclass@gmail.com>
2020-10-27 17:49:10 -0400

    Color scheme name fix and button color initialization fix

---
+++ client/src/styles/ColorSchemes.js
@@ -0,0 +1,28 @@
+// Store data relevant to color schemes and interface stylings.
+
+export const COLOR_SCHEMES = {
+    "Old-School": {
+        "primary": "#e9713d",
+        "secondary": "#eebc90",
+        "accent": "#15253f",
+        "background": "#fae8b7",
+        "hue": "225deg",
+        "buttonColor": "brown",
+    },
+    "Retro": {
+        "primary": "#8b63b1",
+        "secondary": "#e09fcb",
+        "accent": "#010022",
+        "background": "#f6f9ff",
+        "hue": "127deg",
+        "buttonColor": "purple",
+    },
+    "Modern": {
+        "primary": "#3aafa9",
+        "secondary": "#c5e1e0",
+        "accent": "#101a1e",
+        "background": "#feffff",
+        "hue": "0deg",
+        "buttonColor": "teal",
+    },
+}
