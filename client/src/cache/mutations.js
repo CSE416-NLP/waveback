fd12a663dea38cc50b188eb739774acdcebf76b3
unknown <peterateast@gmail.com>
2020-11-04 12:00:48 -0500

    added rest of mutations and queries for user

---
+++ client/src/cache/mutations.js
@@ -0,0 +1,24 @@
+import gql from "graphql-tag";
+
+// ------------- User Mutations --------------//
+
+export const LOGIN = gql`
+    mutation Login($username: String!, $password: String!) {
+        login(username: $username, password: $password) {
+            _id
+            username
+            password
+        }
+    }
+`;
+
+export const REGISTER = gql`
+    mutation Register($username: String!, $email: String!, $password: String!) {
+        register(username: $username, email: $email, password: $password) {
+            _id
+            username
+            email
+            password
+        }
+    }
+`;
