ed7df11399861db656e29611123ee609d25ca05e
TLeonBrown <tianbrown@gmail.com>
2020-11-27 19:24:16 -0500

    Overhaul CSS in Generate screen.

--- client/src/screens/GenerateScreen.js
+++ client/src/screens/GenerateScreen.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import "../styles/css/index.css"
 import { Label } from 'semantic-ui-react'
 
-var buttonStyle = {color: "var(--background)", backgroundColor: "var(--buttonColor"};
-
 const GenerateScreen = (props) => {
   const [numSongs, setNumSongs] = useState(1);
   const [lastSongInputValid, setLastSongInputValid] = useState(true);
@@ -12,6 +10,7 @@ const GenerateScreen = (props) => {
     startYear: "",
     endYear: "",
   }]);
+  const [genreInputState, setGenreInputState] = useState("");
   const [genreState, setGenres] = useState([{
     genre: "",
   }]);
@@ -38,19 +37,26 @@ const GenerateScreen = (props) => {
     setLocations(rowCopy);
   }
 
+  // Called when the user types in a genre in the input field.
   const addNewGenre = () => {
+    let updateThis = true;
     let genreCopy = [...genreState];
-    if (document.getElementById("genreSearch").value.length > 0) {
-      genreCopy.push({
-        genre: document.getElementById("genreSearch").value,
-      });
+    if (genreInputState === "") { return; }
+    for (let i = 0; i < genreCopy.length; i++) {
+      if (genreCopy[i].genre === genreInputState) {updateThis = false; }
     }
+    if (updateThis) { genreCopy.push({genre: genreInputState}); }
     setGenres(genreCopy);
   }
 
+  // Called when the user selects a pre-defined genre button.
   const addGenre = (genreName) => {
+    let updateThis = true;
     let genreCopy = [...genreState];
-    genreCopy.push({genre: genreName});
+    for (let i = 0; i < genreCopy.length; i++) {
+      if (genreCopy[i].genre === genreName) {updateThis = false; }
+    }
+    if (updateThis) { genreCopy.push({genre: genreName}); }
     setGenres(genreCopy);
   }
 
@@ -77,95 +83,101 @@ const GenerateScreen = (props) => {
   return (
     <div className="generateScreen">
       <div className="generateScreenTitleText">bring back the sounds of...</div>
-      <div className="generateScreenModules" style={{backgroundColor: "var(--background)"}}>
-        <p className="generateScreenText">Locations
-          <button style={buttonStyle} className="clickButton ui icon button generateSquareButton" onClick={addLocation}>
-            <i className="plus circle icon"></i>
-          </button>
-          <button style={{color: "var(--background)"}} className="ui grey icon button generateSquareButton" onClick={subtractLocation}>
-            <i className="minus circle icon"></i>
-          </button>
-        </p>
-        <div align="center" className="generateScreenBox" id="gsbLeft" style={{backgroundColor: "var(--background)"}}>
-          <i id="locationText1">Location</i><i id="locationText2">Year Start</i><i id="locationText3">Year End</i>
-          <div className="generateLeftScroller">
-            {locationState.map((row, index) =>
-              <div key={index}>
-                <div className="ui input">
-                  <input value={row.location} className="generateInput" style={{backgroundColor: "var(--secondary)"}} 
-                    onChange={(e) => addLocationInfo(index, "location", e)} />
+      <div className="generateScreenTopContainer">
+        <div className="generateScreenTopInnerContainer">
+          <div className="generateAddSubtractButtons">
+            <h3>Locations</h3>
+            <div className="generateBoxLabel">
+              <button className="clickButton ui icon button generateSquareButton" onClick={addLocation}>
+                <i className="plus circle icon"></i>
+              </button>
+              <button style={{color: "var(--background)"}} className="ui grey icon button generateSquareButton" onClick={subtractLocation}>
+                <i className="minus circle icon"></i>
+              </button>
+            </div> 
+          </div>
+          <div align="center" className="generateScreenBoxLeft">
+            <div className="generateSubText">
+              <i id="lt1">Location</i><i id="lt2">Year Start</i><i id="lt3">Year End</i>
+            </div>
+            <div className="locationScroller">
+              {locationState.map((row, index) =>
+                <div className="locationInfoRow" key={index}>
+                  <div className="ui input">
+                    <input value={row.location} className="generateInput" style={{backgroundColor: "var(--secondary)"}} 
+                      onChange={(e) => addLocationInfo(index, "location", e)} />
+                  </div>
+                  <div className="ui input">
+                    <input maxLength="4" value={row.startYear} className="generateInputSmall" style={{backgroundColor: "var(--secondary)"}} 
+                      onChange={(e) => addLocationInfo(index, "startYear", e)} />
+                  </div>
+                  <div className="ui input">
+                    <input maxLength="4" value={row.endYear} className="generateInputSmall" style={{backgroundColor: "var(--secondary)"}} 
+                      onChange={(e) => addLocationInfo(index, "endYear", e)} />
+                  </div>
                 </div>
-                <div className="ui input">
-                  <input maxLength="4" value={row.startYear} className="generateInputSmall" style={{backgroundColor: "var(--secondary)"}} 
-                    onChange={(e) => addLocationInfo(index, "startYear", e)} />
-                </div>
-                <div className="ui input">
-                  <input maxLength="4" value={row.endYear} className="generateInputSmall" style={{backgroundColor: "var(--secondary)"}} 
-                    onChange={(e) => addLocationInfo(index, "endYear", e)} />
-                </div>
-                <br></br><br></br>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="generateScreenModules" style={{backgroundColor: "var(--background)"}}>
-        <div>
-          <p className="generateScreenText">Genres
-          <button style={buttonStyle} className="clickButton ui icon button generateSquareButton" onClick={addNewGenre}>
-            <i className="plus circle icon"></i>
-          </button>
-            <button style={{color: "var(--background)"}} className="ui icon grey button generateSquareButton" onClick={subtractGenre}>
-              <i className="minus circle icon"></i>
-            </button>
-          </p>
-          <div align="center" className="generateScreenBox" id="gsbRight" style={{backgroundColor: "var(--background)"}}>
-            <div className="genreLeft">
-              <i id="genreText1">Search for Genre</i>
-              <div className="ui input"><input size="20" id="genreSearch" className="generateInputGenre" style={{backgroundColor: "var(--secondary)"}}/></div>
-              <div className="genreButton">
-                <button style={buttonStyle} className="clickButton ui button small" onClick={() => addGenre("Pop")}>Pop</button>
-                <button style={buttonStyle} className="clickButton ui button small" onClick={() => addGenre("Rap")}>Rap</button>
-                <button style={buttonStyle} className="clickButton ui button small" onClick={() => addGenre("R&B")}>R&amp;B</button>
+        <div className="generateScreenTopInnerContainer">
+          <div className="generateAddSubtractButtons">
+            <h3>Genres</h3>
+            <div className="generateBoxLabel">
+              <button className="clickButton ui icon button generateSquareButton" onClick={addNewGenre}>
+                <i className="plus circle icon"></i>
+              </button>
+              <button style={{color: "var(--background)"}} className="ui grey icon button generateSquareButton" onClick={subtractGenre}>
+                <i className="minus circle icon"></i>
+              </button>
+            </div> 
+          </div>
+          <div align="center" className="generateScreenBoxRight">
+            <div className="genreLeftContainer">
+              <div className="generateSubText"><i id="gt1">Search for Genre</i></div>
+              <div className="ui input">
+                <input size="20" id="genreSearch" className="generateInputGenre" onChange={(e) => setGenreInputState(e.target.value)}
+                  style={{backgroundColor: "var(--secondary)"}} 
+                />
               </div>
               <div className="genreButton">
-                <button style={buttonStyle} className="clickButton ui button small" onClick={() => addGenre("Electronic")}>Electronic</button>
-                <button style={buttonStyle} className="clickButton ui button small" onClick={() => addGenre("Alternative")}>Alternative</button>
+                <button className="clickButton ui button small" onClick={() => addGenre("Pop")}>Pop</button>
+                <button className="clickButton ui button small" onClick={() => addGenre("Rap")}>Rap</button>
+                <button className="clickButton ui button small" onClick={() => addGenre("R&B")}>R&amp;B</button>
               </div>
               <div className="genreButton">
-                <button style={buttonStyle} className="clickButton ui button small" onClick={() => addGenre("Rock")}>Rock</button>
-                <button style={buttonStyle} className="clickButton ui button small" onClick={() => addGenre("Indie")}>Indie</button>
-                <button style={buttonStyle} className="clickButton ui button small" onClick={() => addGenre("Jazz")}>Jazz</button>
+                <button className="clickButton ui button small" onClick={() => addGenre("Electronic")}>Electronic</button>
+                <button className="clickButton ui button small" onClick={() => addGenre("Alternative")}>Alternative</button>
               </div>
               <div className="genreButton">
-                <button style={buttonStyle} className="clickButton ui button small" onClick={() => addGenre("Country")}>Country</button>
-                <button style={buttonStyle} className="clickButton ui button small" onClick={() => addGenre("Soul")}>Soul</button>
-                <button style={buttonStyle} className="clickButton ui button small" onClick={() => addGenre("Folk")}>Folk</button>
+                <button className="clickButton ui button small" onClick={() => addGenre("Rock")}>Rock</button>
+                <button className="clickButton ui button small" onClick={() => addGenre("Indie")}>Indie</button>
+                <button className="clickButton ui button small" onClick={() => addGenre("Jazz")}>Jazz</button>
               </div>
               <div className="genreButton">
-                <button style={buttonStyle} className="clickButton ui button small" onClick={() => addGenre("Classical")}>Classical</button>
-                <button style={buttonStyle} className="clickButton ui button small" onClick={() => addGenre("Ambient")}>Ambient</button>
+                <button className="clickButton ui button small" onClick={() => addGenre("Country")}>Country</button>
+                <button className="clickButton ui button small" onClick={() => addGenre("Soul")}>Soul</button>
+                <button className="clickButton ui button small" onClick={() => addGenre("Folk")}>Folk</button>
               </div>
-
+              <div className="genreButton">
+                <button className="clickButton ui button small" onClick={() => addGenre("Classical")}>Classical</button>
+                <button className="clickButton ui button small" onClick={() => addGenre("Ambient")}>Ambient</button>
+              </div>
             </div>
-            <div className="generateRightScroller">
+            <div className="genreRightContainer">
               {genreState.map((row, index) =>
-                <p key={index}>
-                  <i className="genreList">{row.genre}</i>
-                </p>
+                <p key={index}><i className="genreList">{row.genre}</i></p>
               )}
             </div>
           </div>
         </div>
       </div>
 
-      <div className="generateDivider">
-        <div className="ui divider"></div>
-      </div>
+      <div className="generateDivider"></div>
 
       <div className="generateBottomArea">
-          <p className="generateScreenText">Playlist Size</p>
+          <h3>Playlist Size</h3>
           <div className="ui input maxSongInputArea">
             <button className="ui button grey icon" onClick={() => changeNumSongs(numSongs - 1)}>
               <i className="angle left icon"/>
@@ -178,7 +190,7 @@ const GenerateScreen = (props) => {
             </button>
           </div>
           <div className="generateButtonBox">
-            <button style={buttonStyle}  className="clickButton ui button massive generateButton">GENERATE!</button>
+            <button className="clickButton ui button massive generateButton">GENERATE!</button>
           </div>
         </div>
 
