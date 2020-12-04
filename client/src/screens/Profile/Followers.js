93b1062deb12ac17218fb89c1563ef42f9e1f690
NathanWine <illseeyouafterclass@gmail.com>
2020-12-03 23:10:51 -0500

    Added filtering to Followers and Following components

--- client/src/screens/Profile/Followers.js
+++ client/src/screens/Profile/Followers.js
@@ -1,54 +1,26 @@
 import React, { useState }  from 'react';
-import { Grid, Button, Icon, Modal, Header } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import "../../styles/css/index.css";
-import { graphql } from '@apollo/react-hoc';
-import { flowRight as compose } from 'lodash';
-import { GETUSERBYUSERNAME } from '../../cache/mutations';
 import jsonData from "../../data/TestData.json";
-import UserSearch from "../../UtilityComponents/UserSearch";
-
 
 const Followers = (props) => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [userSearchOpenState, setUserSearchOpenState] = useState(false);
+  const [filter, setFilter] = useState("");
   const users = jsonData.Users;
   const columns = 2;
-
-  const searchUser = async (searchTerm) => {
-    const {data} = await props.getuserbyusername({ variables: { username: searchTerm}})
-    // console.log(data);
-    if (data && data.getUserByUsername) {
-      console.log(data.getUserByUsername);
-      if (data.getUserByUsername.length === 0) {
-        console.log("No followers found");
-        return;
-      }
-      props.history.push({ pathname: "/profile/" + data.getUserByUsername[0]._id, user: data.getUserByUsername[0] });
-    }
-    else
-      console.log("No followers found");
-  }
-
-  const handleSearchInput = (event) => {
-    setSearchTerm(event.target.value);
-    if (event.key === 'Enter') {
-        searchUser(event.target.value);
-    }
-  }
 
   return (
     <div className="profileScreenMainContainerFollowing">
       <div className="followingSearchContainer ui input">
       <input placeholder="Filter followers..." style={{ backgroundColor: "var(--secondary)" }} size="50" className="discoverSearch"
-           onKeyUp={(e) => handleSearchInput(e)} />
-        <button type="submit" className="clickButton ui icon big button" onClick={() => searchUser(searchTerm)}>
+           onChange={(e) => setFilter(e.target.value)} />
+        <button type="submit" className="clickButton ui icon big button">
           <i className="search icon"></i>
         </button>
       </div>
       <div className="profileScreenScrollContainer">
         <Grid columns={columns} divided>
-          {users.map((user, index) => (
+          {users.filter(user => user.username.toLowerCase().substring(0, filter.length).includes(filter.toLowerCase())).map((user, index) => (
             <Grid.Column width={Math.floor(16 / columns)} key={index}>
               <Link className="profileScreenFollowing" to={{ pathname: "/profile/" + user._id, user: user }}>
                 <img className="profilePicture" src={user.profilePicture} alt="" />
@@ -58,31 +30,10 @@ const Followers = (props) => {
               </Link>
             </Grid.Column>
           ))}
-          <Modal
-            onClose={() => setUserSearchOpenState(false)}
-            onOpen={() => setUserSearchOpenState(true)}
-            open={Boolean(userSearchOpenState)}
-            size='large'
-            trigger={
-              <Grid.Column width={Math.floor(16 / columns)}>
-              <div className="searchForUserButton">
-                <button className="clickButton ui massive button">Search for User</button>
-              </div>
-            </Grid.Column>}>
-            <Header icon>Find New User</Header>
-            <Modal.Content>
-                <UserSearch {...props}></UserSearch>
-            </Modal.Content>
-            <Modal.Actions className="recoverPasswordModalButtonContainer">
-                <Button inverted color='red' onClick={(e) => setUserSearchOpenState(false)}><Icon name='close' />Close</Button>
-            </Modal.Actions>
-          </Modal>
         </Grid>
       </div>
     </div>
   )
 } 
-export default compose(
-  (graphql(GETUSERBYUSERNAME, { name: 'getuserbyusername' }))
-)(Followers);
+export default Followers;
 
--- client/src/screens/Profile/Following.js
+++ client/src/screens/Profile/Following.js
@@ -4,31 +4,16 @@ import { Link } from 'react-router-dom';
 import "../../styles/css/index.css";
 import { graphql } from '@apollo/react-hoc';
 import { flowRight as compose } from 'lodash';
-import { GETUSERBYUSERNAME, FOLLOWUSER } from '../../cache/mutations';
+import { FOLLOWUSER } from '../../cache/mutations';
 import jsonData from "../../data/TestData.json";
 import UserSearch from "../../UtilityComponents/UserSearch";
 
 const Following = (props) => {
   const user = props.user
-  const [filterTerm, setFilterTerm] = useState("");
-  const [searchTerm, setSearchTerm] = useState("");
+  const [filter, setFilter] = useState("");
   const [userSearchOpenState, setUserSearchOpenState] = useState(false);
   const users = jsonData.Users;
   const columns = 2;
-
-  const searchUser = async (searchTerm) => {
-    const { data } = await props.getuserbyusername({ variables: { username: searchTerm } })
-    if (data && data.getUserByUsername) {
-      console.log(data.getUserByUsername);
-      if (data.getUserByUsername.length === 0) {
-        console.log("No following users found");
-        return;
-      };
-      props.history.push({ pathname: "/profile/" + data.getUserByUsername[0]._id, user: data.getUserByUsername[0] });
-    }
-    else
-      console.log("No following users found")
-  }
 
   const followUser = async () => {
     console.log("About to follow lebron", user._id);
@@ -37,30 +22,16 @@ const Following = (props) => {
     else console.log("couldn't add lebron")
   }
 
-  const handleSearchInput = (event) => {
-    setSearchTerm(event.target.value);
-    if (event.key === 'Enter') {
-        searchUser(event.target.value);
-    }
-  }
-
   return (
     <div className="profileScreenMainContainerFollowing">
       <div className="followingSearchContainer ui input">
-        <input placeholder="Filter users..." style={{ backgroundColor: "var(--secondary)" }} size="50" className="discoverSearch"
-           onKeyUp={(e) => handleSearchInput(e)} />
-        <button type="submit" className="clickButton ui icon big button" onClick={() => searchUser(searchTerm)}>
-          <i className="search icon"></i>
-        </button>
-      </div>
-      <div className="followingSearchContainer ui input">
         <input placeholder="Filter following..." style={{ backgroundColor: "var(--secondary)" }} size="50" className="discoverSearch"
-          value={filterTerm} onChange={(e) => setFilterTerm(e.target.value)} />
+          value={filter} onChange={(e) => setFilter(e.target.value)} />
         <button onClick={followUser}>follow lebron</button>
       </div>
       <div className="profileScreenScrollContainer">
         <Grid columns={columns} divided>
-          {users.map((user, index) => (
+          {users.filter(user => user.username.toLowerCase().substring(0, filter.length).includes(filter.toLowerCase())).map((user, index) => (
             <Grid.Column width={Math.floor(16 / columns)} key={index}>
               <Link className="profileScreenFollowing" to={{ pathname: "/profile/" + user._id, user: user }}>
                 <img className="profilePicture" src={user.profilePicture} alt="" />
@@ -77,16 +48,16 @@ const Following = (props) => {
             size='large'
             trigger={
               <Grid.Column width={Math.floor(16 / columns)}>
-              <div className="searchForUserButton">
-                <button className="clickButton ui massive button">Search for User</button>
-              </div>
-            </Grid.Column>}>
+                <div className="searchForUserButton">
+                  <button className="clickButton ui massive button">Search for User</button>
+                </div>
+              </Grid.Column>}>
             <Header icon>Find New User</Header>
             <Modal.Content>
-                <UserSearch {...props}></UserSearch>
+              <UserSearch {...props}></UserSearch>
             </Modal.Content>
             <Modal.Actions className="recoverPasswordModalButtonContainer">
-                <Button inverted color='red' onClick={(e) => setUserSearchOpenState(false)}><Icon name='close' />Close</Button>
+              <Button inverted color='red' onClick={(e) => setUserSearchOpenState(false)}><Icon name='close' />Close</Button>
             </Modal.Actions>
           </Modal>
         </Grid>
@@ -95,6 +66,5 @@ const Following = (props) => {
   )
 }
 export default compose(
-  (graphql(GETUSERBYUSERNAME, { name: 'getuserbyusername' })),
   (graphql(FOLLOWUSER, { name: 'followuser' }))
 )(Following);
